da062a8511b972eb3faaf29eb5b509ca689ac6c2
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-09-10 22:05:15 -0300

    fix: add next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://crochetstoreapi.onrender.com/:path*', // Proxy para o backend
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://crochetstoreapi.onrender.com/:path*', // Proxy para o backend
+      },
+    ];
+  },
+};
+
+export default nextConfig;
