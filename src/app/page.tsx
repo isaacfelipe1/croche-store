498069cbd1d9ae457c79e0f87c0cc10dea622566
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-09-12 04:09:52 -0300

    feat: add descrição para feito por encomenda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,137 +1,148 @@
-"use client";
-import React, { useEffect, useState, memo } from 'react';
-import { FaWhatsapp, FaHeart, FaRegHeart, FaSearch, FaSpinner } from 'react-icons/fa'; // Adicionando FaSpinner
-import { getProducts, Product } from '../app/api';
-import CategoryFilter from '../app/components/categoryFilter';
-import ImageModal from '../app/components/imageModal';
-import Alert from '../app/components/alert';
-import { parseCookies } from 'nookies';
-import Image from 'next/image';
+'use client'
+import React, { useEffect, useState, memo } from 'react'
+import {
+  FaWhatsapp,
+  FaHeart,
+  FaRegHeart,
+  FaSearch,
+  FaSpinner,
+} from 'react-icons/fa' // Adicionando FaSpinner
+import { getProducts, Product } from '../app/api'
+import CategoryFilter from '../app/components/categoryFilter'
+import ImageModal from '../app/components/imageModal'
+import Alert from '../app/components/alert'
+import { parseCookies } from 'nookies'
+import Image from 'next/image'
+
 const ProductsList: React.FC = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-  const [selectedCategory, setSelectedCategory] = useState<string>('');
-  const [searchTerm, setSearchTerm] = useState<string>('');
-  const [visibleCount, setVisibleCount] = useState<number>(6);
-  const [isImageModalOpen, setIsImageModalOpen] = useState<boolean>(false);
+  const [products, setProducts] = useState<Product[]>([])
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
+  const [selectedCategory, setSelectedCategory] = useState<string>('')
+  const [searchTerm, setSearchTerm] = useState<string>('')
+  const [visibleCount, setVisibleCount] = useState<number>(6)
+  const [isImageModalOpen, setIsImageModalOpen] = useState<boolean>(false)
   const [currentImage, setCurrentImage] = useState<{
-    url: string;
-    alt: string;
-  } | null>(null);
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const [alertMessage, setAlertMessage] = useState<string | null>(null);
-  const [purchaseFeedback, setPurchaseFeedback] = useState<string | null>(null);
-  const [favoriteProducts, setFavoriteProducts] = useState<number[]>([]);
+    url: string
+    alt: string
+  } | null>(null)
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+  const [alertMessage, setAlertMessage] = useState<string | null>(null)
+  const [purchaseFeedback, setPurchaseFeedback] = useState<string | null>(null)
+  const [favoriteProducts, setFavoriteProducts] = useState<number[]>([])
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const data = await getProducts();
+        const data = await getProducts()
         const formattedData = data.map((product) => ({
           ...product,
           price: parseFloat(product.price.toString()),
-        }));
-        setProducts(formattedData);
-        setFilteredProducts(formattedData);
+        }))
+        setProducts(formattedData)
+        setFilteredProducts(formattedData)
       } catch (error) {
-        setError('Erro ao carregar produtos');
+        setError('Erro ao carregar produtos')
       } finally {
-        setLoading(false);
+        setLoading(false)
       }
-    };
+    }
 
-    fetchProducts();
+    fetchProducts()
 
-    const cookies = parseCookies();
-    const token = cookies.token;
-    setIsLoggedIn(!!token);
+    const cookies = parseCookies()
+    const token = cookies.token
+    setIsLoggedIn(!!token)
 
     const savedFavorites = JSON.parse(
       localStorage.getItem('favoriteProducts') || '[]',
-    );
-    setFavoriteProducts(savedFavorites);
+    )
+    setFavoriteProducts(savedFavorites)
 
     const handleStorageChange = () => {
-      const cookies = parseCookies();
-      const updatedToken = cookies.token;
-      setIsLoggedIn(!!updatedToken);
-    };
+      const cookies = parseCookies()
+      const updatedToken = cookies.token
+      setIsLoggedIn(!!updatedToken)
+    }
 
-    window.addEventListener('storage', handleStorageChange);
+    window.addEventListener('storage', handleStorageChange)
 
     return () => {
-      window.removeEventListener('storage', handleStorageChange);
-    };
-  }, []);
+      window.removeEventListener('storage', handleStorageChange)
+    }
+  }, [])
 
   useEffect(() => {
-    let filtered = products;
+    let filtered = products
     if (selectedCategory) {
       filtered = filtered.filter(
         (product) => product.category === selectedCategory,
-      );
+      )
     }
     if (searchTerm) {
       filtered = filtered.filter((product) =>
         product.name.toLowerCase().includes(searchTerm.toLowerCase()),
-      );
+      )
     }
 
-    setFilteredProducts(filtered);
-  }, [selectedCategory, searchTerm, products]);
-
+    setFilteredProducts(filtered)
+  }, [selectedCategory, searchTerm, products])
 
   const toggleFavorite = (productId: number) => {
     if (!isLoggedIn) {
-      setAlertMessage('Você precisa estar logado para favoritar um produto.');
-      return;
+      setAlertMessage('Você precisa estar logado para favoritar um produto.')
+      return
     }
 
     const updatedFavorites = favoriteProducts.includes(productId)
       ? favoriteProducts.filter((id) => id !== productId)
-      : [...favoriteProducts, productId];
+      : [...favoriteProducts, productId]
 
-    setFavoriteProducts(updatedFavorites);
-    localStorage.setItem('favoriteProducts', JSON.stringify(updatedFavorites));
-  };
+    setFavoriteProducts(updatedFavorites)
+    localStorage.setItem('favoriteProducts', JSON.stringify(updatedFavorites))
+  }
 
   const handleWhatsappPurchase = (product: Product) => {
-    const cookies = parseCookies();
-    const token = cookies.token;
+    const cookies = parseCookies()
+    const token = cookies.token
 
     if (!token) {
-      setAlertMessage('Você precisa estar logado para realizar a compra.');
-      return;
+      setAlertMessage('Você precisa estar logado para realizar a compra.')
+      return
     }
 
-    const phoneNumber = '5592991921009';
-    const message = `Olá!\nEstou interessado em comprar o produto: *${product.name}*\nCor: _${product.color}_\nPreço: *R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}*\nO produto é este: ${product.imageUrl}`;
-    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    window.open(whatsappURL, '_blank');
+    const phoneNumber = '5592991921009'
+    const message = `Olá!\nEstou interessado em comprar o produto: *${product.name}*\nCor: _${product.color}_\nPreço: *R$${product.price.toLocaleString(
+      'pt-BR',
+      { minimumFractionDigits: 2, maximumFractionDigits: 2 },
+    )}*\nO produto é este: ${product.imageUrl}`
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+      message,
+    )}`
+    window.open(whatsappURL, '_blank')
 
-    setPurchaseFeedback('Redirecionando para o WhatsApp...');
-    setTimeout(() => setPurchaseFeedback(null), 3000);
-  };
+    setPurchaseFeedback('Redirecionando para o WhatsApp...')
+    setTimeout(() => setPurchaseFeedback(null), 3000)
+  }
 
   const handleShowMore = () => {
-    setVisibleCount((prevCount) => prevCount + 6);
-  };
+    setVisibleCount((prevCount) => prevCount + 6)
+  }
 
   const openImageModal = (imageUrl: string, altText: string) => {
-    setCurrentImage({ url: imageUrl, alt: altText });
-    setIsImageModalOpen(true);
-  };
+    setCurrentImage({ url: imageUrl, alt: altText })
+    setIsImageModalOpen(true)
+  }
 
   const closeImageModal = () => {
-    setCurrentImage(null);
-    setIsImageModalOpen(false);
-  };
+    setCurrentImage(null)
+    setIsImageModalOpen(false)
+  }
 
   const categories = Array.from(
     new Set(products.map((product) => product.category)),
-  );
+  )
 
   if (loading)
     return (
@@ -139,8 +150,8 @@ const ProductsList: React.FC = () => {
         <FaSpinner className="animate-spin text-4xl text-[#432721]" />
         <p className="ml-2 text-center text-lg mt-4">Carregando produtos...</p>
       </div>
-    );
-  if (error) return <p className="text-center text-red-500 mt-4">{error}</p>;
+    )
+  if (error) return <p className="text-center text-red-500 mt-4">{error}</p>
 
   return (
     <div className="container mx-auto px-4 py-8">
@@ -188,7 +199,9 @@ const ProductsList: React.FC = () => {
                     className="w-full h-48 object-cover transition-transform duration-300 ease-in-out transform hover:scale-105"
                     width={500}
                     height={500}
-                    onClick={() => openImageModal(product.imageUrl, product.name)}
+                    onClick={() =>
+                      openImageModal(product.imageUrl, product.name)
+                    }
                   />
                   <button
                     className="absolute top-2 right-2 text-[#E56446] hover:text-[#432721] transition-colors duration-200"
@@ -209,21 +222,37 @@ const ProductsList: React.FC = () => {
                     <p className="text-base text-[#432721] mb-1 font-semibold">
                       Preço:{' '}
                       <span className="text-[#E56446] font-bold text-xl">
-                        R${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                        R$
+                        {product.price.toLocaleString('pt-BR', {
+                          minimumFractionDigits: 2,
+                          maximumFractionDigits: 2,
+                        })}
                       </span>
                     </p>
+                    {typeof product.description === 'string' &&
+                      product.description.trim() === 'Feito por encomenda' && (
+                        <p className="text-sm text-[#432721] mb-2 font-bold">
+                          {product.description}
+                        </p>
+                      )}
+
                     <p className="text-sm text-[#432721] mb-2 font-bold">
                       Cor: <span className="font-medium">{product.color}</span>
                     </p>
                     <p className="text-sm text-[#432721] mb-2 font-bold">
-                      Quantidade: <span className="font-medium">{product.stockQuantity} Un</span>
+                      Quantidade:{' '}
+                      <span className="font-medium">
+                        {product.stockQuantity} Un
+                      </span>
                     </p>
                     {product.stockQuantity < 2 ? (
                       <p className="text-[#E56446] font-bold text-sm mb-2">
                         Compre já, poucas unidades!
                       </p>
                     ) : (
-                      <p className="text-[#61B785] font-bold text-sm mb-2">Em estoque</p>
+                      <p className="text-[#61B785] font-bold text-sm mb-2">
+                        Em estoque
+                      </p>
                     )}
                   </div>
                   <button
@@ -258,7 +287,7 @@ const ProductsList: React.FC = () => {
         />
       )}
     </div>
-  );
-};
+  )
+}
 
-export default memo(ProductsList);
+export default memo(ProductsList)
