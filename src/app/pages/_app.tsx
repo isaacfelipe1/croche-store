ece70bc56606be96e4a5dd2712a39769bace47b5
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-09-12 21:43:55 -0300

    feat: adicionada o google anality

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
-import Layout from '../layout'; 
+import Layout from '../layout';
+import Script from 'next/script';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Inicializar o Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag(...args: any) {
+      window.dataLayer.push(args);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-RKLDDZR4WL'); // Substitua pelo seu ID da métrica
+  }, []);
+
   return (
     <Layout>
+      {/* Script do Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-RKLDDZR4WL`} // Substitua pelo seu ID da métrica
+      />
       <Component {...pageProps} />
     </Layout>
   );
