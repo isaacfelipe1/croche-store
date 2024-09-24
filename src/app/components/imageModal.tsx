1fd9a5dac55c5802d2aab042f7fc18dac4e04a1d
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-09-24 04:07:10 -0300

    feat: colocando zoom nas imagem

--- src/app/components/imageModal.tsx
+++ src/app/components/imageModal.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Modal from './modal';
 import Image from 'next/image';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import { FaSearchPlus, FaSearchMinus } from 'react-icons/fa';
 
 interface ImageModalProps {
   isOpen: boolean;
@@ -13,18 +14,43 @@ interface ImageModalProps {
 const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onClose, imageUrl, altText }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="flex justify-center items-center animate-fadeIn">
+      <div className="flex flex-col items-center justify-center animate-fadeIn">
         <TransformWrapper>
-          <TransformComponent>
-            <Image
-              src={imageUrl}
-              alt={altText}
-              width={1000}
-              height={1000}
-              className="max-w-full max-h-[80vh] object-contain"
-              loading="lazy"
-            />
-          </TransformComponent>
+          {({ zoomIn, zoomOut, resetTransform }) => (
+            <>
+              <div className="flex justify-between w-full mb-2 space-x-4">
+                {/* Botões de controle de zoom */}
+                <button
+                  onClick={() => zoomIn()}  
+                  className="p-2 bg-gray-200 rounded-full hover:bg-gray-300"
+                >
+                  <FaSearchPlus size={20} />
+                </button>
+                <button
+                  onClick={() => zoomOut()}  
+                  className="p-2 bg-gray-200 rounded-full hover:bg-gray-300"
+                >
+                  <FaSearchMinus size={20} />
+                </button>
+                <button
+                  onClick={() => resetTransform()}  
+                  className="p-2 bg-gray-200 rounded-full hover:bg-gray-300"
+                >
+                  Reset
+                </button>
+              </div>
+              <TransformComponent>
+                <Image
+                  src={imageUrl}
+                  alt={altText}
+                  width={1000}
+                  height={1000}
+                  className="max-w-full max-h-[80vh] object-contain"
+                  loading="lazy"
+                />
+              </TransformComponent>
+            </>
+          )}
         </TransformWrapper>
       </div>
     </Modal>
