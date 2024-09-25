c2eabfc0399fcdbb346b061a24d73ec0227e009d
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-09-25 05:24:27 -0300

    feat: otimizando as imagem

--- src/app/components/imageModal.tsx
+++ src/app/components/imageModal.tsx
@@ -3,7 +3,7 @@ import Modal from './modal';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 import { FaSearchPlus } from 'react-icons/fa';
-
+import Image from 'next/image';
 interface ImageModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -17,11 +17,11 @@ const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onClose, imageUrl, altT
       <div className="flex flex-col items-center justify-center animate-fadeIn">
         <div className="overflow-hidden flex justify-center items-center max-w-full max-h-screen">
           <Zoom>
-            <img
+            <Image
               src={imageUrl}
               alt={altText}
-              className="max-w-full max-h-[80vh] object-contain"  // Limita o tamanho da imagem e mantém a proporção
-              style={{ width: 'auto', height: 'auto' }}  // Garante que a imagem se ajuste corretamente
+              className="max-w-full max-h-[80vh] object-contain"  
+              style={{ width: 'auto', height: 'auto' }}  
             />
           </Zoom>
         </div>
