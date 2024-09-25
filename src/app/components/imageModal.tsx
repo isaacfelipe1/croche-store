8861abe91c63af8d82d9d3164fb24f916f7a7e05
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-09-25 04:43:34 -0300

    feat: segunda versão modal

--- src/app/components/imageModal.tsx
+++ src/app/components/imageModal.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Modal from './modal';
-import InnerImageZoom from 'react-inner-image-zoom';
-import 'react-inner-image-zoom/lib/InnerImageZoom/styles.min.css';
+import Zoom from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css';
+import { FaSearchPlus } from 'react-icons/fa';
 
 interface ImageModalProps {
   isOpen: boolean;
@@ -15,14 +16,23 @@ const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onClose, imageUrl, altT
     <Modal isOpen={isOpen} onClose={onClose}>
       <div className="flex flex-col items-center justify-center animate-fadeIn">
         <div className="overflow-hidden flex justify-center items-center max-w-full max-h-screen">
-          <InnerImageZoom
-            src={imageUrl}
-            zoomSrc={imageUrl} // Imagem de alta resolução para o zoom
-            alt={altText}
-            className="max-w-full max-h-[80vh] object-contain"  // Limita o tamanho da imagem
-            zoomType="hover"  // Ou "click" para ampliar ao clicar
-          />
+          <Zoom>
+            <img
+              src={imageUrl}
+              alt={altText}
+              className="max-w-full max-h-[80vh] object-contain"  // Limita o tamanho da imagem e mantém a proporção
+              style={{ width: 'auto', height: 'auto' }}  // Garante que a imagem se ajuste corretamente
+            />
+          </Zoom>
         </div>
+
+        {/* Botão de zoom */}
+        <button
+          className="mt-4 p-2 bg-gray-200 rounded-full hover:bg-gray-300 flex items-center"
+        >
+          <FaSearchPlus size={20} />
+          <span className="ml-2">Ampliar Imagem</span>
+        </button>
       </div>
     </Modal>
   );
