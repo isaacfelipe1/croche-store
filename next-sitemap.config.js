d25b8229cef4b1a3dd063aed5bdfbcc862e68a51
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-10-03 01:41:31 -0300

    feat: reajuste para indexação

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,17 @@
-// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: 'https://croche-store.vercel.app/', // Substitua pelo seu domínio
+  siteUrl: 'https://croche-store.vercel.app/', // Seu domínio
   generateRobotsTxt: true, // Gera automaticamente o arquivo robots.txt
-  // Outras configurações conforme necessário
+  exclude: ['/editar', '/excluir', '/listar'], // Exclui essas páginas do sitemap e do robots.txt
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/', // Permite a indexação da página inicial
+        disallow: ['/editar', '/excluir', '/listar'], // Bloqueia a indexação das páginas específicas
+      },
+    ],
+  },
 };
 
 module.exports = config;
