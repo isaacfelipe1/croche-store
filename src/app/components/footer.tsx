ca96aa72fd9fb355373ecd5b972c47bee6dc6a33
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2025-01-25 23:20:33 -0300

    feat: add o ano atual no footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import { FaFacebook } from 'react-icons/fa';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear(); 
+
   return (
     <footer className="bg-[#432721] text-white text-center py-5 w-full mt-auto">
       <p className="text-[#F1E4A6]">
-        &copy; 2024 loja de crochê. - Todos os direitos reservados.
+        &copy; {currentYear} loja de crochê. - Todos os direitos reservados.
       </p>
       <p className="text-[#E56446] hover:text-[#61B785] transition-colors duration-300">
         Desenvolvido para você comprar seus produtos!
       </p>
       <div className="flex justify-center mt-4">
-        <a href="https://www.facebook.com/people/Amor-Croch%C3%AA/100095593387200/?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
-          <FaFacebook className="text-[#F1E4A6] hover:text-[#61B785] transition-colors duration-300" size={24} />
+        <a
+          href="https://www.facebook.com/people/Amor-Croch%C3%AA/100095593387200/?mibextid=ZbWKwL"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebook
+            className="text-[#F1E4A6] hover:text-[#61B785] transition-colors duration-300"
+            size={24}
+          />
         </a>
       </div>
     </footer>
